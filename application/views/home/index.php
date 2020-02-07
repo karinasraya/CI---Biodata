be4318c7629140ff20da1e9bf4a547865ae3e41b
karinasraya <karinasoraya.ks@gmail.com>
2020-02-08 06:08:38 +0700

    Tugas 1 PBKK

---
+++ application/views/home/index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Biodata Karina</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md  fixed-top maine-menu">
+    <div class="container">
+      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"> <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
+      <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#about" tabindex="-1" aria-disabled="true">About</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#activity" tabindex="-1" aria-disabled="true">Hobby</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#portfolio" tabindex="-1" aria-disabled="true">Portfolio</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#contact" tabindex="-1" aria-disabled="true">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid fh5co-home-banner">
+    <div class="card"> <img class="card-img" src="assets/img/banner-img.jpg">
+      <div class="card-img-overlay">
+        <div class="center-text">
+          <h2 class="card-title">KARINA SORAYA PUSPITASARI</h2>
+          <a href="https://www.instagram.com/karinasraya/" class="btn">
+            <svg width="201" height="51" viewBox="0 0 201 51">
+              <defs>
+                <style>
+                .cls-1 {
+                  fill: none;
+                  stroke-width: 1px;
+                  stroke: url(#linear-gradient);
+                }
+              </style>
+              <linearGradient id="linear-gradient" x1="140.508" y1="50.5" x2="60.492" y2="0.5" gradientUnits="userSpaceOnUse">
+                <stop offset="0" stop-color="#e90e65"/>
+                <stop offset="1" stop-color="#fff"/>
+              </linearGradient>
+            </defs>
+            <rect id="Rounded_Rectangle_1" data-name="Rounded Rectangle 1" class="cls-1" x="0.5" y="0.5" width="200" height="50" rx="25" ry="25"/>
+          </svg>
+        Follow Me !!</a> </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid fh5co-two-img">
+    <div class="row">
+      <div class="col-sm-6 pr-0 pl-0">
+        <div class="card"> <img class="card-img" src="assets/img/group1.png" alt="">
+          <div class="card-img-overlay"> </div>
+        </div>
+      </div>
+      <div class="col-sm-6 pr-0 pl-0">
+        <div class="card"> <img class="card-img" src="assets/img/group2.png" alt="">
+          <div class="card-img-overlay"> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid fh5co-recent-work">
+    <div class="container contact-pop">
+      <div class="row">
+        <div class="col-md-6  pr-0">
+          <div class="card"> <img class="card-img w-100" src="assets/img/group3.png" alt="">
+            <div class="card-img-overlay"> </div>
+          </div>
+        </div>
+        <div class="col-md-6 pl-0" id="about">
+          <div class="content">
+            <h3>Karina Soraya P</h3>
+            <h4>05111740000003</h4>
+            <hr />
+            <p>Seorang gadis kecil yang senang berpetualang ini terlahir di Jember, 23 April 2000. Saat ini saya mengenyam pendidikan di Departemen Teknik Informatika ITS.</p>
+            <a href="https://api.whatsapp.com/send?phone=62895411218946" class="btn">CONTACT</a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="container recent" id="activity">
+        <div class="row">
+          <h2>My Hobby</h2>
+          <div class="owl-carousel owl-carousel2 owl-theme">
+            <div>
+              <div class="card"> <img class="card-img" src="assets/img/group4.png" alt="">
+                <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+                  <div class="bottom-text">
+                    <h5 class="card-title">Watch Movie</h5>
+                    <p class="card-text">Action, Horror</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="card"> <img class="card-img" src="assets/img/group6.png" alt="">
+                <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+                  <div class="bottom-text">
+                    <h5 class="card-title">Listening to Music</h5>
+                    <p class="card-text">Indie, Pop</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="card"> <img class="card-img" src="assets/img/group5.png" alt="">
+                <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+                  <div class="bottom-text">
+                    <h5 class="card-title">Tour and potrait</h5>
+                    <p class="card-text">Digital, Photography</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="card"> <img class="card-img" src="assets/img/group7.png" alt="">
+                <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+                  <div class="bottom-text">
+                    <h5 class="card-title">Culinary</h5>
+                    <p class="card-text">Indonesian Food</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid recent fh5co-portfolio" id="portfolio">
+    <div class="container">
+      <h2>Portfolios</h2>
+      <div class="row">
+        <div class="bx bx-1">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img1.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Natural site</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bx bx-2">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img2.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Vacation special photography</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bx bx-3">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img3.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Product photography</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bx bx-4">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img4.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Historical place shots</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bx bx-4">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img4.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Historical place shots</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bx bx-4">
+          <div class="card"> <img class="card-img" src="assets/img/portfolio-img4.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+              <div class="bottom-text">
+                <h5 class="card-title">Historical place shots</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid fh5co-recent-work activity">
+    <div class="container recent">
+      <div class="row">
+        <h2>Activity of the month</h2>
+        <div class="owl-carousel owl-carousel3 owl-theme">
+          <div>
+            <div class="card"> <img class="card-img" src="assets/img/group8.png" alt="">
+              <div class="card-img-overlay">
+                <div class="bottom-text">
+                  <h5 class="card-title">Assistant Lecturer</h5>
+                  <a href="#">Read more <img src="assets/img/double-right.svg" alt=""></a> 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card"> <img class="card-img" src="assets/img/group9.png"  alt="">
+              <div class="card-img-overlay">
+                <div class="bottom-text">
+                  <h5 class="card-title">College Student</h5>
+                  <a href="#">Read more <img src="assets/img/double-right.svg" alt=""></a> 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card"> <img class="card-img" src="assets/img/group10.png" alt="">
+              <div class="card-img-overlay">
+                <div class="bottom-text">
+                  <h5 class="card-title">Organization</h5>
+                  <a href="#">Read more <img src="assets/img/double-right.svg" alt=""></a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid fh5co-insta-feed activity">
+    <div class="container recent">
+      <h2 class="text-center d-block">Find me on social media</h2>
+      <div class="row social-links">
+        <ul class="nav mx-auto">
+          <li class="nav-item"> <a class="nav-link" href="https://www.facebook.com/karinasraya"><img src="assets/img/facebook.png" alt=""></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="https://twitter.com/karinasrayaa"><img src="assets/img/twitter.png" alt=""></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="mailto:karinasoraya.ks@gmail.com"><img src="assets/img/google-plus.png" alt=""></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <footer class="container-fluid p-0 pr-0">
+    <div class="row mr-0 ml-0">
+      <div class="col-md-6 pr-0 pl-0 map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126625.31377234691!2d112.70665330026088!3d-7.349286768753592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa0cd4b725bb%3A0xc7c50c56bc6c0b46!2sBumi%20Madina%20Asri!5e0!3m2!1sid!2sid!4v1580983464506!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </div>
+      <div class="col-md-6 content-us">
+        <div class="contact-form" id="contact">
+          <h3 class="text-uppercase">Contact me</h3>
+          <input type="text" class="form-control" placeholder="Your Name">
+          <input type="text" class="form-control" placeholder="Your Email">
+          <textarea class="form-control" placeholder="Your Message"></textarea>
+          <button type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+    <div class="copy pt-4 pb-4">
+      <p><a href="https://freehtml5.co/" target="_blank"> &copy; 2019 Karina Soraya P</a></p>
+    </div>
+  </footer>
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS --> 
+  <script src="assets/js/jquery-3.3.1.slim.min.js"></script> 
+  <script src="assets/js/popper.min.js" ></script> 
+  <script src="assets/js/bootstrap.min.js"></script> 
+  <script src="assets/js/owl.carousel.min.js"></script> 
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
